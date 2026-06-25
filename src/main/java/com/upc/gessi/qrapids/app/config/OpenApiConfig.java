0da50023b23167f5857b42c81599f3788ca04144
EncryptEx <41539618+EncryptEx@users.noreply.github.com>
2026-06-25 22:08:25 +0200

    add apikey doc on swagger

--- src/main/java/com/upc/gessi/qrapids/app/config/OpenApiConfig.java
+++ src/main/java/com/upc/gessi/qrapids/app/config/OpenApiConfig.java
@@ -2,12 +2,16 @@ package com.upc.gessi.qrapids.app.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String API_KEY_SCHEME = "LD-API-Key";
 
     @Bean
     public GroupedOpenApi learningDashboardApi() {
@@ -23,6 +27,11 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Learning Dashboard Lite API")
                         .description("OpenAPI documentation for the Learning Dashboard Lite REST API.")
-                        .version("3.3"));
+                        .version("3.3"))
+                .schemaRequirement(API_KEY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("X-LD-API-Key"))
+                .addSecurityItem(new SecurityRequirement().addList(API_KEY_SCHEME));
     }
 }
